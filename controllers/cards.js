73133363f7ee475ce17d8b3c6141b918d4dc1740
Kristina <kristinafurman@proton.me>
2023-08-15 14:18:16 +0300

    place-gha

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,6 +3,7 @@ const Card = require('../models/card');
 
 module.exports.addCard = (req, res) => {
   const { name, link } = req.body;
+  console.log('addCard', name, link)
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
       Card.findById(card._id)
@@ -31,12 +32,14 @@ module.exports.getCards = (req, res) => {
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
-      res.send(card);
+      if (card) {
+        res.status(200).send(card)
+      } else { throw new Error('DocumentNotFoundError') }
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
         res.status(400).send({ message: `Некоректный _id: ${req.params.cardId} карточки` });
-      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      } else if (err.message === 'DocumentNotFoundError') {
         res.status(404).send({
           message: `Карточка по указанному _id: ${req.params.cardId} не найдена`,
         });
@@ -50,12 +53,14 @@ module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .populate(['owner', 'likes'])
     .then((card) => {
-      res.send(card);
+      if (card) {
+        res.status(200).send(card)
+      } else { throw new Error('DocumentNotFoundError') }
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
         res.status(400).send({ message: `Некоректный _id: ${req.params.cardId} карточки` });
-      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      } else if (err.message === 'DocumentNotFoundError') {
         res.status(404).send({
           message: `Карточка по указанному _id: ${req.params.cardId} не найдена`,
         });
@@ -69,12 +74,14 @@ module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .populate(['owner', 'likes'])
     .then((card) => {
-      res.send(card);
+      if (card) {
+        res.status(200).send(card)
+      } else { throw new Error('DocumentNotFoundError') }
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
         res.status(400).send({ message: `Некоректный _id: ${req.params.cardId} карточки` });
-      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      } else if (err.message === 'DocumentNotFoundError') {
         res.status(404).send({
           message: `Карточка по указанному _id: ${req.params.cardId} не найдена`,
         });
