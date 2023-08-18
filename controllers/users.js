3ad0823afd74d6b90d4a00875d0ec306494288e7
Kristina <kristinafurman@proton.me>
2023-08-18 23:27:42 +0300

    place-gha

--- controllers/users.js
+++ controllers/users.js
@@ -42,15 +42,15 @@ module.exports.getUserById = (req, res) => {
 module.exports.editUserData = async (req, res) => {
   const { name, about } = req.body;
   try {
-    const user = await User.findByIdAndUpdate(req.body._id, { name, about }, { new: 'true', runValidators: true })
+    const user = await User.findByIdAndUpdate(req.user._id, { name, about }, { new: 'true', runValidators: true })
       .orFail();
     res.status(200).send(user);
   } catch (err) {
     if (err instanceof mongoose.Error.CastError) {
-      res.status(400).send({ message: `Некоректный id: ${req.body._id}` });
+      res.status(400).send({ message: `Некоректный id: ${req.user._id}` });
     } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
       res.status(404).send({
-        message: `Пользователь по указанному _id: ${req.body._id} не найден`,
+        message: `Пользователь по указанному _id: ${req.user._id} не найден`,
       });
     } else {
       res.status(500).send({ message: 'На сервере произошла ошибка' });
