b3339b30982ae3c48d74bb0fc07effdc8ef541cc
Kristina <kristinafurman@proton.me>
2023-09-01 23:42:42 +0300

    place-gha

--- routes/cards.js
+++ routes/cards.js
@@ -16,7 +16,7 @@ router.delete('/:cardId', celebrate({
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().pattern(urlRegex),
+    link: Joi.string().required().pattern(urlRegex),
     // link: Joi.string().default('').uri(),
   }),
 }), addCard);
