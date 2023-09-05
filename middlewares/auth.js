376e539151110cbdefcfd1a66ecf2424eb243400
Kristina <kristinafurman@proton.me>
2023-09-05 21:57:29 +0300

    place-gha

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,3 @@
-const { HTTP_STATUS_UNAUTHORIZED } = require('http2').constants;
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
@@ -7,11 +6,9 @@ const UnauthorizedError = require('../errors/Unauthorized');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) { res.status(HTTP_STATUS_UNAUTHORIZED).send(); }
-  if (err instanceof mongoose.Error.UnauthorizedError) {
-    next(new UnauthorizedError('Необходима авторизация')); // 401
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация'); // 401
   }
-  next(err);
 
   const token = authorization.replace('Bearer ', '');
   let payload;
